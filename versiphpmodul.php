82629803e213aaa5d809a172f8398890fab9e9c7
DoniNugroho <doni.n@students.amikom.ac.id>
2016-11-20 11:52:37 +0700

    upload3

---
+++ versiphpmodul.php
@@ -0,0 +1,14 @@
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+
+//edited
+echo 'SUDAH DIEDIT!!';
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
